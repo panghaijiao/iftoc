9038219c468afcf843b3c1950ca8b09f7c23b254
庞海礁 <275742376@qq.com>
2017-01-06 19:43:36 +0800

    0.1.9

---
+++ iconfont/Iconfont.swift
@@ -0,0 +1,18 @@
+//
+//  Iconfont.swift
+//  AutoCoding
+//
+//  Created by AutoCoding on 2017/01/06.
+//  Copyright © 2017年 olinone. All rights reserved.
+//
+
+import UIKit
+
+public func iconFontWithSize(size: CGFloat) -> UIFont {
+    return UIFont(name: "iconfont", size: size)!
+}
+
+let iconall = "\u{e696}"
+
+let iconsmile = "\u{e6af}"
+
